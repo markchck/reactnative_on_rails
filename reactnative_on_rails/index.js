71097ffe747e209f97ccaad7a652012243f190a5
markchck <markchck@gmail.com>
2022-05-28 15:54:22 +0900

    aa

--- reactnative_on_rails/index.js
+++ reactnative_on_rails/index.js
@@ -11,7 +11,8 @@ function Index() {
         // http는 reactnative에서 안받아지나봄.. 아래 https는 받아지는데 http는 계속 axioserror: network error 뜸. 이건 아예 axios가 서버에 접근도 못했다는 에러임.
         axios.get("https://jsonplaceholder.typicode.com/posts/1")
         // axios.get("https://markchck.github.io/reactnative_on_rails/notes.json")
-        .then(res => setNotes(JSON.stringify(res.data.title)))
+        // .then(res => setNotes(JSON.stringify(res.data.title)))
+        .then(res => setNotes("hi"))
         .catch(res => console.log(res))
       }, [notes.length]
   )

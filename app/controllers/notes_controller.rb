0a2a65703816c36a8e553aefb20a2686159d8fe6
markchck <markchck@gmail.com>
2022-05-30 23:31:38 +0900

    css완료

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,24 +1,49 @@
 class NotesController < ApplicationController
+  # before_action :authenticate_user!
   def index
     notes = Note.all
     # render json: notes, status: :ok
     render json: NoteSerializer.new(notes).serializable_hash.to_json
   end
-
-  def create
-    notes = Note.create!(note_params)
+  
+  def show
+    notes = Note.find(params[:id])
     render json: NoteSerializer.new(notes).serializable_hash.to_json
     # render json: notes, status: :ok
   end
-
-  def show
-    notes = Note.find_by(id: params[:id])
+  
+  def create
+    # notes = Note.create!(text: params[:text])
+    notes = Note.create(note_params)
     render json: NoteSerializer.new(notes).serializable_hash.to_json
-    # render json: notes, status: :ok
+  end
+
+  def update
+    notes = Note.find(params[:id])
+
+    # 1. 이 방법도 되고
+    # notes.text = params[:text]
+    # notes.save
+    
+    # 2. 이 방법도 되는데
+    notes.update(note_params)
+    # notes.update(text: parmas[:text])    
+
+    # 3. 이건 에러 뜸
+    # updated_notes = notes.update(note_params)
+    # render json: NoteSerializer.new(updated_notes).serializable_hash.to_json
+
+    render json: NoteSerializer.new(notes).serializable_hash.to_json
+  end
+
+  def destroy
+    # notes = Note.find(params[:id])
+    # notes.destroy
+    Note.destroy(params[:id])
   end
 
   private
-  def note_params
-    params.require(:note).permit(:text)
-  end
+    def note_params
+      params.require(:note).permit(:text)
+    end
 end
--- reactnative_on_rails/index.js
+++ reactnative_on_rails/index.js
@@ -1,23 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
 
 function Index() {
   const [notes, setNotes] = useState([])
   useEffect( ()=> {
-        axios.get("http://localhost:3000/notes/")
-        // 안드로이드가 http는 막아놔서 웹에서는 정상 출력되는데 앱에서는 출력이 안될거임. https로 바꿔서 넣으면 출력 됨.
-        // axios.get("https://jsonplaceholder.typicode.com/posts/1")
-        .then(res => {
-          // alert(JSON.stringify(res))
-          setNotes(JSON.stringify(res.data))
-        })
-        .catch(res => {
-          alert(JSON.stringify(res.data))
-        })
-      }, [notes.length]
+      axios.get("http://localhost:3000/notes/")
+      // 안드로이드가 http는 막아놔서 웹에서는 정상 출력되는데 앱에서는 출력이 안될거임. https로 바꿔서 넣으면 출력 됨.
+      // axios.get("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => {
+        //이터레이팅이 필요함.
+        setNotes(JSON.stringify(res.data.data[0].attributes.text))
+        
+      })
+      .catch(res => {
+        alert(JSON.stringify(res.data))
+      })
+    }, [notes.length]
   )
 
   //axios말고 fetch함수로 api 받아보기
@@ -28,8 +28,28 @@ function Index() {
 
   return (
       <View style={styles.container}>
-        <Text>{notes}</Text>
         <StatusBar style="auto"/>  
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Text>work</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>travel</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.body}>
+          <View style={styles.input}>
+            <TextInput 
+              placeholder='hi'
+              style={styles.textInput}
+            />
+          </View>
+
+          <View style={styles.output}>
+            <Text>{notes}</Text>
+          </View>
+
+        </View>
       </View>
   );
 }
@@ -37,10 +57,30 @@ function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  header: {
+    flexDirection: 'row',
+    marginTop: 30,
+    marginBottom: 50,
+    backgroundColor: 'green',
+    justifyContent: 'space-around',
+  },
+  body: {
+    backgroundColor: 'tomato',
+
+  },
+  input: {
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    // justifyContent: 'flex-end',
+  },
+  textInput:{
+    backgroundColor: 'black'
+  },
+  output: {
+    backgroundColor: 'blue'
+  }
 });
 
 

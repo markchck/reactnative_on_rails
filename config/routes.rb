efc9179c8316420e7290e74fd73c25dd92b883e5
markchck <markchck@gmail.com>
2022-06-11 13:31:08 +0900

    av

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'notes#index'
-  resources :notes
+  
+  # root 'notes#index'
+  # resources :notes
+
+  namespace  :api do
+    namespace :v1 do
+      resources :notes
+    end
+  end
+
 end
